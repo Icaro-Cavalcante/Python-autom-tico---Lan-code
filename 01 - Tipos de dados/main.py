a911642f406d1095bd6df5ca786a26cae0e6fc93
Icaro-Cavalcante <icarodev06@gmail.com>
2025-10-10 10:09:39 -0300

    01 - Tipos de dados

---
+++ 01 - Tipos de dados/main.py
@@ -0,0 +1,22 @@
+saudacao = "Hello, world!" # Isso é uma string, uma cadeia de caracteres
+print(type(saudacao))
+idade = 18 # Isso é um INT, um número inteiro
+print(type(idade))
+preco = 12.99 # Isso é um float, um número flutuante
+print(type(preco))
+python = True # Isso é um boolean, um dado lógico que tem 2 valores [verdadeiro (true) e falso (false)]
+print(type(python))
+
+# Temos também os operadores aritméticos
+
+print(1 + 2) # adição
+print(8 - 6) # subtração
+print(40 / 2) # divisão
+print(50 // 11) # divisão inteira
+print(40 % 2) # resto da divisão
+print(2**6) # potenciação
+
+# Dentro disso temos a prioridade dos operadores:
+#   1 - **
+#   2 - /, //, *, %
+#   3 - +, -
