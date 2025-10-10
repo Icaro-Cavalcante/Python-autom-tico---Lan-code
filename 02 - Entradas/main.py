39cbd0f9c07cd0c6c8baa494daaaa8daf9e2ccd6
Icaro-Cavalcante <icarodev06@gmail.com>
2025-10-10 10:19:55 -0300

    02 - Entradas

---
+++ 02 - Entradas/main.py
@@ -0,0 +1,7 @@
+# Para aceitar as entradas/inputs do usuário usamos a fução input
+nome = str(input("Digite seu nome: "))
+print(f"Olá {nome}") # input: Icaro | output: Olá Icaro
+x = int(input("Atribua um valor a x: "))
+y = int(input("Atribua um valor a y: "))
+soma = x + y
+print(soma)
