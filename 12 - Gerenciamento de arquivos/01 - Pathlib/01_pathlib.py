7426ca598fec7953ab3563b8cb3faaba27f81879
Icaro-Cavalcante <icarodev06@gmail.com>
2025-11-01 13:54:07 -0300

    12 - pathlib completo

--- 12 - Gerenciamento de arquivos/01 - Pathlib/01_pathlib.py
+++ 12 - Gerenciamento de arquivos/01 - Pathlib/01_pathlib.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-caminho = Path("12 - Gerenciamento de arquivos/01 - Pathlib/mensagem.txt") # Caminho relativo
+caminho = Path(r"12 - Gerenciamento de arquivos/01 - Pathlib/mensagem.txt") # Caminho relativo
 print(caminho)
-absolute = Path(r"/homeyora/Documentos/GitHub/Python-autom-tico---Lan-code/12 - Gerenciamento de arquivos/01 - Pathlib/mensagem.txt") # Caminho absoluto
+absolute = Path(r"C:\Users\anail\OneDrive\Documentos\Códigos\dvdvd\Python-autom-tico---Lan-code\12 - Gerenciamento de arquivos\01 - Pathlib\mensagem.txtt") # Caminho absoluto
 absolute = caminho.absolute() # Podemos transformar um caminho relativo em absoluto
 
 if caminho.exists: # verifica se existe
@@ -28,8 +28,25 @@ nova_pasta.mkdir(exist_ok=True, parents=True) # make dir (fazer pasta) | o (exis
 # caminho.unlink() # Remove arquivos
 nova_pasta.rmdir() # Remove pastas
 
-caminho = Path("mensagem.txt")
-texto = caminho.read_text()
+caminho = Path(r"12 - Gerenciamento de arquivos\01 - Pathlib\mensagem.txt")
+texto = caminho.read_text() # lê texto
 print(texto)
 
-caminho.write_text("Python")
+caminho.write_text("Python é legal.", encoding="utf-8") # Escreve texto no arquivo | enconding="utf-8" para aceitar caracteres especiais
+
+pasta = Path(r"12 - Gerenciamento de arquivos\01 - Pathlib\pasta")
+pasta = Path(r"12 - Gerenciamento de arquivos\01 - Pathlib")
+# for file in pasta.iterdir():
+#     print(file)
+for file in pasta.glob("*.txt"): # filtra pelo tipo do arquivo
+    print(file)
+
+caminho = Path(r"12 - Gerenciamento de arquivos\01 - Pathlib\01_pathlib.py")
+print("-" * 20)
+print(caminho) # caminho
+print(caminho.name) # nome completo
+print(caminho.stem) # nome sem o sufixo
+print(caminho.suffix) # sufixo
+
+caminho = Path(r"12 - Gerenciamento de arquivos\01 - Pathlib\arquivo.txt")
+caminho.touch() # cria o arquivo
